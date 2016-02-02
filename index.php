9973fdfd97e6525088173272ecc3b87c2010d2e5
motzart <8etaj@ukr.net>
2016-02-02 10:04:09 +0200

    more update

--- index.php
+++ index.php
@@ -5,24 +5,24 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 
     <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/handlebars-v4.0.5.js"></script>
     <script src="js/app.js"></script>
-    <script src="js/handlebars-v4.0.5.js"></script
 
     <meta charset="UTF-8">
     <title>Album</title>
 </head>
 <body>
 
-
 <div class="container">
-    <h1><a href="#">All Albums</a></h1>
+    <h1><a id="link" href="/">All Albums</a></h1>
     <div id="album"></div>
 </div>
 
 
 <div id="slideout">
     <div id="slideout_inner">
-        <a href="#/add">+ Add Album</a>
+        <a id="link" href="/add" class="btn btn-default btn-block">Add Album</a>
+        <button onClick="clearLocalStorage()" type="submit" class="btn btn-default btn-block">Clear all album</button>
     </div>
 </div>
 
@@ -32,7 +32,8 @@
     <div class="row">
         {{#each updates}}
         <div class="col-md-3">
-            <h1><a href="#album/{{id}}">{{title}}</a></h1>
+            <h1><a id="link" href="/album/{{id}}">{{title}}</a></h1>
+            <h3>{{author}}</h3>
             <h3>{{type}}</h3>
             <div class="body">
                 {{image}}
@@ -52,27 +53,91 @@
                 <label for="exampleInputPassword1">Author</label>
                 <input type="text" class="form-control" id="InputAuthor" placeholder="Author">
             </div>
+            <div class="text-center">
+
+            </div>
             <div class="form-group">
                 <label for="InputFile">Cover</label>
-                <input type="file" id="InputFile">
+                <input type="file" name="cover" id="cover">
                 <p class="help-block">add cover for album</p>
             </div>
-            <div class="radio">
-                <label>
-                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                    mp3
-                </label>
-            </div>
-            <div class="radio">
-                <label>
-                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option2">
-                    video
-                </label>
-            </div>
 
-            <button type="submit" class="btn btn-default">Submit</button>
+            <div id="imageCover" class="text-center"></div>
+            <button id="submitForm" type="submit" class="btn btn-default">Submit</button>
 
         </form>
     </div>
+
+</script>
+<script id="single-album" type="text/x-handlebars-template">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>{{title}}</h1>
+            <h3>{{author}}</h3>
+            <div class="body">
+                {{image}}
+            </div>
+        </div>
+    </div>
+</script>
+<script>
+
+    saveImage();
+    var nId = 1;
+    $('body').on('click', '#submitForm', function (e) {
+        e.preventDefault();
+        nId++;
+        var form = $('#album-form');
+        var Title = $('#InputTitle').val();
+        var Author = $('#InputAuthor').val();
+        if ($("#InputTitle").val() == '') {
+
+            $('#alert').html("<strong>Warning!</strong>You left title entry");
+            $('#alert').fadeIn().delay(1000).fadeOut();
+            return false;
+        }
+
+        form.trigger('reset');
+
+        var album = {
+            id: nId,
+            title: Title,
+            author: Author,
+            file: null
+        };
+
+        var jsonAlbum = JSON.stringify(album);
+        localStorage.setItem(nId, jsonAlbum);
+        return false;
+    });
+
+    function getAlbum(id) {
+        return localStorage.getItem(id);
+    }
+
+
+    function saveImage() {
+        $('body').on("change", '#cover', function () {
+            var fileInput = $(this)[0];
+            var file = fileInput.files[0];
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                var img = new Image();
+                img.src = reader.result;
+                localStorage.theImage = reader.result; //stores the image to localStorage
+            };
+
+            reader.readAsDataURL(file);
+
+        });
+    }
+
+    function clearLocalStorage() {
+        window.localStorage.clear();
+        location.reload();
+        return false;
+    }
+
 </script>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,68 +1,74 @@
+var Router = {
+    URL: window.location.href,
 
-var routes = {};
+    routes: {
+        "http://album.loc/"          :  "indexPage",
+        "http://album.loc/add"       :  "addPage",
+        "http://album.loc/album/:id" :  "albumPage"
+    },
 
-function route (path, templateId, controller) {
-    routes[path] = {templateId: templateId, controller: controller};
+    init: function () {
+        this._routes = [];
+        for (var route in this.routes) {
+            var methodName = this.routes[route];
+            this._routes.push({
+                pattern: new RegExp('^' + route.replace(/:\w+/, '(\\w+)') + '$'),
+                callback: this[methodName]
+            });
+        }
+    },
+    nav: function (path) {
+        var i = this._routes.length;
+        while (i--) {
+            var args = path.match(this._routes[i].pattern);
+            console.log(args);
+            if (args) {
+                this._routes[i].callback.apply(this, args.slice(1));
+            }
+        }
+    },
+
+    indexPage: function () {
+        var source = $('#all-albums').html();
+        var template = Handlebars.compile(source);
+        var html = template(getAllAlbumFromLS());
+        $('#album').html(html);
+    },
+
+    addPage: function () {
+        var source = $('#add-albums').html();
+        var template = Handlebars.compile(source);
+        $('#album').html(template);
+    },
+
+    albumPage: function (id) {
+        var source = $('#single-album').html();
+        var template = Handlebars.compile(source);
+        console.log(localStorage.getItem(id));
+        var html = template(localStorage.getItem(id));
+        $('#album').html(html);
+
+    }
+};
+
+function getAllAlbumFromLS() {
+
+    var updates = [];
+
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var obj = JSON.parse(localStorage.getItem(key));
+        updates.push(obj);
+    }
+
+    return {updates: updates};
 }
 
-var el = null;
-function router () {
-
-    el = el || document.getElementById('album');
-
-    var url = location.hash.slice(1) || '/';
-
-    var route = routes[url];
-
-    if (el && route.controller) {
-
-        route.templateId;
-        new route.controller();
-    }
-}
-
-window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
-
-
-route('/', 'home', function () {
-    var source = $('#all-albums').html();
-    var template = Handlebars.compile(source);
-    var html = template(data);
-    $('#album').html(html);
+$(document).ready(function () {
+    Router.init();
+    Router.nav(window.location.href);
+    $("body").on("click", "a#link", function (evt) {
+        Router.nav(evt.currentTarget.href);
+        evt.preventDefault();
+    });
 });
-
-route('/add', 'add', function () {
-    var source = $('#add-albums').html();
-    var template = Handlebars.compile(source);
-    var html = template(data);
-    $('#album').html(html);
-});
-
-
-var data = { updates : [
-    {
-        id : 1,
-        title: 'title album',
-        type: 'type album',
-        image: 'image album'
-    },
-    {
-        id : 2,
-        title: 'title album2',
-        type: 'type album2',
-        image: 'image album2'
-    },
-    {
-        id : 3,
-        title: 'title album3',
-        type: 'type album3',
-        image: 'image album3'
-    },
-    {
-        id : 4,
-        title: 'title album4',
-        type: 'type album4',
-        image: 'image album4'
-    }
-]};
